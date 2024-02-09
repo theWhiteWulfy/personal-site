72de4ccd2b08adea999aeda1645e53236ad8ecc0
Alok <i@alokprateek.in>
2024-02-10 01:14:27 +0530

    nextjs core

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import "@/styles/global.css";
+import NextNprogress from 'nextjs-progressbar';
+
+export default function App({ Component, pageProps }) {
+  return (
+  
+  <Component {...pageProps} />
+  
+  );
+}
