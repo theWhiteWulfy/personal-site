55488d8ab401895c9a9ce4b31cfceec7403854cc
Alok <i@alokprateek.in>
2024-06-14 23:10:40 +0530

    Add reading time and modified time

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
+import { remarkModifiedTime } from './src/lib/remark-modified-time.mjs';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
   integrations: [sitemap(), mdx(), pagefind()],
   markdown: {
-    shikiConfig: {
-      theme: "css-variables",
-    },
+    syntaxHighlight: 'prism',
+    remarkPlugins: [remarkReadingTime, remarkModifiedTime],
   },
 });
