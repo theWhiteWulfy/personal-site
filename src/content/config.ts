d7f9ff5732d33b5280b50cb0217b9c37094bf10b
Alok <i@alokprateek.in>
2024-06-21 11:37:15 +0530

    add collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
+// TEST COLLECTION
+
 const blog = defineCollection({
   type: "content",
   schema: z.object({
@@ -32,4 +34,107 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, projects };
+// REAL COLLECTIONS
+
+const articles = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    path: z.string(),
+    date: z.coerce.date(),
+    last_modified_at: z.coerce.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    hide_meta: z.boolean().optional(),
+    comments: z.boolean().optional(),
+    comments_locked: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+const notes = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    path: z.string(),
+    date: z.coerce.date(),
+    last_modified_at: z.coerce.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    hide_meta: z.boolean().optional(),
+    comments: z.boolean().optional(),
+    comments_locked: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+const works = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    path: z.string(),
+    date: z.coerce.date(),
+    last_modified_at: z.coerce.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    hide_meta: z.boolean().optional(),
+    comments: z.boolean().optional(),
+    comments_locked: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+const illustrations = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    path: z.string(),
+    date: z.coerce.date(),
+    last_modified_at: z.coerce.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    hide_meta: z.boolean().optional(),
+    comments: z.boolean().optional(),
+    comments_locked: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+const bibliophilediaries = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    path: z.string(),
+    date: z.coerce.date(),
+    last_modified_at: z.coerce.date(),
+    excerpt: z.string(),
+    image: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    toc: z.boolean().optional(),
+    hide_meta: z.boolean().optional(),
+    comments: z.boolean().optional(),
+    comments_locked: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+
+export const collections = { blog, projects , articles, notes, works, illustrations, bibliophilediaries };
