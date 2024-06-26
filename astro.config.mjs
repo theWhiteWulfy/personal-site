2f85cbdd79245e5acc405c40dc1c549ec2fe056e
Alok <i@alokprateek.in>
2024-06-26 17:31:25 +0530

    add playform compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 import { remarkModifiedTime } from './src/lib/remark-modified-time.mjs';
 
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [sitemap(), mdx(), pagefind(), playformCompress()],
   markdown: {
     syntaxHighlight: 'prism',
-    remarkPlugins: [remarkReadingTime, remarkModifiedTime],
-  },
+    remarkPlugins: [remarkReadingTime, remarkModifiedTime]
+  }
 });
