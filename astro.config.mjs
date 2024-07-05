6618dc6e7153f455540edc224cf9a25b0b120cc1
Alok <i@alokprateek.in>
2024-07-06 02:07:47 +0530

    add vite pwa imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { VitePWA } from "vite-plugin-pwa"
 //import pagefind from "astro-pagefind";
+import { manifest } from "./src/config/manifest";
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 import { remarkModifiedTime } from './src/lib/remark-modified-time.mjs';
 
