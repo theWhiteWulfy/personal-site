cfc71d6c90575a00ffbdcc65b36955514228f248
Alok <i@alokprateek.in>
2024-07-15 23:39:09 +0530

    add cloudflare server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,37 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { VitePWA } from "vite-plugin-pwa"
+import { VitePWA } from "vite-plugin-pwa";
 //import pagefind from "astro-pagefind";
 import { manifest } from "./src/config/manifest";
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 import { remarkModifiedTime } from './src/lib/remark-modified-time.mjs';
-
 import playformCompress from "@playform/compress";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://meteoricteachings.pages.dev/",
-  integrations: [sitemap(), mdx(), playformCompress()], //pagefind(), 
+  integrations: [sitemap(), mdx(), playformCompress()],
+  //pagefind(), 
   markdown: {
     syntaxHighlight: 'prism',
     remarkPlugins: [remarkReadingTime, remarkModifiedTime]
   },
   vite: {
-    plugins: [
-      VitePWA({
-        registerType: "autoUpdate",
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: [
-            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-          ],
-          // Don't fallback on document based (e.g. `/some-page`) requests
-          // This removes an errant console.log message from showing up.
-          navigateFallback: null,
-        },
-      })
-    ]
-  }
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
+  },
+  output: "server",
+  adapter: cloudflare()
 });
