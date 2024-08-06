87e04222b036b0f798b15de585272f5c1cadf17e
Alok <i@alokprateek.in>
2024-08-07 01:19:17 +0530

    Add D1 to env config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+
+type D1Database = import("@cloudflare/workers-types").D1Database;
+type ENV = {
+    DB: D1Database;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+    interface Locals extends Runtime { }
+}
