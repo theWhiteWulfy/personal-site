4299db9481fccbca883d631da140f59cd3b119e1
Alok <i@alokprateek.in>
2024-08-08 16:28:27 +0530

    update newsletter api

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,9 +1,19 @@
 import type { APIRoute, APIContext } from 'astro';
+export const prerender = false;
 
-export const POST: APIRoute = async ({ request }, {locals}: APIContext) => {
+export const POST: APIRoute = async ({ request, locals}: APIContext) => {
     const formData = await request.formData();
     const email = formData.get('subsemail');
+    console.log('form:', formData);
+    console.log('Email:', email);
+    console.log('Locals:', locals);
+
+    if (!locals || !locals.runtime || !locals.runtime.env || !locals.runtime.env.DB) {
+        return new Response('Database not configured', { status: 500 });
+    }
+
     const { DB } = locals.runtime.env;
+    
     if (typeof email !== 'string') {
         return new Response('Invalid email', { status: 400 });
     }
@@ -14,4 +24,3 @@ export const POST: APIRoute = async ({ request }, {locals}: APIContext) => {
 
     return new Response('Email submitted successfully', { status: 200 });
 };
-
