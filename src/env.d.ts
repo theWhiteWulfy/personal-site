c18d57174a74ecfe7b6f48bf188dc4a01ac1bf6c
Alok Prateek <i@alokprateek.in>
2026-09-17 01:14:11 +0530

    chore: add R2 bucket binding for resources

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,10 @@
 
 
 type D1Database = import("@cloudflare/workers-types").D1Database;
+type R2Bucket = import("@cloudflare/workers-types").R2Bucket;
 type ENV = {
     DB: D1Database;
+    RESOURCES_BUCKET: R2Bucket;
 };
 
 // use a default runtime configuration (advanced mode).
